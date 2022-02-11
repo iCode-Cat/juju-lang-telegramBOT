125f88224f691380ddae065ca5202089b680809b
iCode-Cat <devranboyac8@gmail.com>
2022-02-11 12:27:07 +0200

    puppeteer settings change

--- webScrapper/webScrap.js
+++ webScrapper/webScrap.js
@@ -17,7 +17,6 @@ async function makeWebScrapper({ services }) {
         'div.ddef_h > div';
       const browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: true,
       });
       const page = await browser.newPage();
       await page.goto(domain + language + word);
